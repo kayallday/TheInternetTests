8e3530b603ab738cf67def3b92fe294aee54fe1e
KayBeard <kbeard@apterainc.com>
2017-11-21 14:56:54 -0500

    feat: created page objects and introduced the BasePageObject

---
+++ LoginTest.cs/Tests/LoginTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PageObjects;
+
+namespace LoginTest.cs
+{
+    [TestFixture]
+    public class LoginTest
+    {
+        IWebDriver Driver;
+        LoginPage Login;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Driver = new ChromeDriver();
+            Login = new LoginPage(Driver); 
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+
+        [Test]
+        public void ValidAccount()
+        {
+            Login.With("tomsmith", "SuperSecretPassword!");
+            Assert.That(Login.SuccessMessagePresent);
+        }
+
+        [Test]
+        public void BadPasswordProvided()
+        {
+            Login.With("tomsmith", "SuperSecretPasswad!");
+            Assert.That(Login.FailureMessagePresent);
+        }
+     }
+}
